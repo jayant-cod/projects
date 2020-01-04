96d38cb1cdb0133b2f6c47dc38dc507d32fbbd2a
jayant-cod <55781883+jayant-cod@users.noreply.github.com>
2020-01-05 00:11:22 +0530

    Add files via upload

---
+++ Brick_game/Brick_maker.java
@@ -0,0 +1,54 @@
+package Brick_game;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Brick_maker 
+{
+	public int map[][];
+	public int brickWidth;
+	public int brickHeight;
+	
+	//defining rows and column 
+	public Brick_maker (int row, int col)
+	{
+		map = new int[row][col];		
+		for(int i = 0; i<map.length; i++)
+		{
+			for(int j =0; j<map[0].length; j++)
+			{
+				map[i][j] = 1;
+			}			
+		}
+		
+		brickWidth = 540/col;
+		brickHeight = 150/row;
+	}	
+	
+	public void draw(Graphics2D g)
+	{
+		for(int i = 0; i<map.length; i++)
+		{
+			for(int j =0; j<map[0].length; j++)
+			{
+				if(map[i][j] > 0)
+				{
+					g.setColor(Color.blue);
+					//start with 80,50
+					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+					
+					// for adding stroke
+					g.setStroke(new BasicStroke(3));
+					g.setColor(Color.black);
+					g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);				
+				}
+			}
+		}
+	}
+	
+	public void setBrickValue(int value, int row, int col)
+	{
+			map[row][col] = value;
+	}
+}
